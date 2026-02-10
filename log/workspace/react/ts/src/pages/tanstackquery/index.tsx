ed3382eef6f62d7c9fe509b3ad23090d5df4a8ed
4BFC <4bee.code@gmail.com>
2026-02-10 23:46:04 +0900

    feat: 간단한 interface 구현

--- log/workspace/react/ts/src/pages/tanstackquery/index.tsx
+++ log/workspace/react/ts/src/pages/tanstackquery/index.tsx
@@ -1,5 +1,28 @@
+import { useUsers, useCreateUser } from "../../hooks/useUsers"
+
 function Page(){
-    return <div>test</div>
+    const { data, isLoading, error } = useUsers(1)
+    const createUser = useCreateUser()
+
+    if (isLoading) return <div>Loading...</div>
+    if (error) return <div>Error!</div>
+
+    return (
+      <div>
+        <h1>Users</h1>
+        <ul>
+          {data?.data.map((user) => (
+            <li key={user.id}>{user.firstName} {user.lastName}</li>
+          ))}
+        </ul>
+
+        <button
+          onClick={() => createUser.mutate({ name: 'poby', job: 'developer' })}
+        >
+          Add User
+        </button>
+      </div>
+    )
 }
 
 export default Page
